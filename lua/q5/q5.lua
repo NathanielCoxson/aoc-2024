353d5e2c2f285eed05116b11aae6e940d19edaa2
Nathan Coxson <nathancoxson2739@gmail.com>
2024-12-07 18:52:38 -0500

    added day five part two solution

--- lua/q5/q5.lua
+++ lua/q5/q5.lua
@@ -1,4 +1,19 @@
 local enablePrint = false
+
+local function printList(l)
+    for i, v in pairs(l) do
+        io.write(v)
+        if i < #l then io.write(" ") end
+    end
+    io.write("\n")
+end
+
+local function getTableLength(t)
+    local length = 0
+    for _, _ in pairs(t) do length = length + 1 end
+    return length
+end
+
 local function getInput(filename)
     local f = assert(io.open(filename, "r"))
     local d = {}
@@ -53,30 +68,42 @@ local function checkUpdate(u, r)
     return true
 end
 
-local function orderRules(r)
-    local after = {}
+local function getRuleMap(r)
+    local map = {}
     for _, rule in pairs(r) do
         local x, y = rule[1], rule[2]
-        if after[x] == nil then after[x] = {} end
-        after[x][y] = true
+        if map[x] == nil then map[x] = {} end
+        table.insert(map[x], y)
     end
-    for k, x in pairs(after) do
-        io.write(k, ": ")
-        for y, _ in pairs(x) do
-            io.write(y, ",")
+    return map
+end
+
+local function searchRule(r, x, y)
+    for i, t in pairs(r) do
+        for _, j in pairs(t) do
+            if i == x and j == y then return true end
         end
-        io.write("\n")
     end
+    return false
 end
 
 local function fixUpdate(u, r)
-
+    for i = 1, getTableLength(u) do
+        for j = i + 1, getTableLength(u) do
+            if not searchRule(r, u[i], u[j]) then
+                local temp = u[i]
+                u[i] = u[j]
+                u[j] = temp
+            end
+        end
+    end
+    return u
 end
 
-local data = getInput("q5.test.txt")
+local data = getInput("q5.in.txt")
 local rules = getRules(data)
 local updates = getUpdates(data)
-orderRules(rules)
+local ruleMap = getRuleMap(rules)
 
 local invalidSum = 0
 for _, update in pairs(updates) do
@@ -86,3 +113,12 @@ for _, update in pairs(updates) do
     end
 end
 print(invalidSum)
+
+local correctedSum = 0
+for _, update in pairs(updates) do
+    if not checkUpdate(update, rules) then
+        update = fixUpdate(update, ruleMap)
+        correctedSum = correctedSum + update[#update // 2 + 1]
+    end
+end
+print(correctedSum)
