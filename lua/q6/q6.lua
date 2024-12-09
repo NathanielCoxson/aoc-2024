ab0acd42e4236678393004b9933cf1f005ea4236
Nathan Coxson <nathancoxson2739@gmail.com>
2024-12-09 18:16:31 -0500

    added day six part two solution

--- lua/q6/q6.lua
+++ lua/q6/q6.lua
@@ -10,6 +10,17 @@ local function getLines(filename)
     return lines
 end
 
+local function copyMap(m)
+    local output = {}
+    for i, t in pairs(m) do
+        output[i] = {}
+        for j, v in pairs(t) do
+            output[i][j] = v
+        end
+    end
+    return output
+end
+
 local function getMap(lines)
     local map = {}
 
@@ -35,6 +46,7 @@ local function getStartingPoint(m)
     end
 end
 
+-- Part 1
 local function dfs(m, r, c, d)
     local ROWS, COLS = #m, #m[1]
     local map = m
@@ -67,6 +79,65 @@ local function dfs(m, r, c, d)
     return map
 end
 
+-- Part 2
+local function checkForCycle(m, r, c, d)
+    local ROWS, COLS = #m, #m[1]
+    local map = m
+    local stack = {{r, c}}
+    local seen = {}
+
+    while #stack > 0 do
+        local row, col = stack[#stack][1], stack[#stack][2]
+        local cycleFound = seen[row..","..col..","..d]
+        if cycleFound then return true end
+
+        map[row][col] = "X"
+        table.remove(stack, #stack)
+        seen[row..","..col..","..d] = true
+
+        local dr, dc = row, col
+        if     d == 0 then dr = dr - 1
+        elseif d == 1 then dc = dc + 1
+        elseif d == 2 then dr = dr + 1
+        elseif d == 3 then dc = dc - 1 end
+
+        if (1 <= dr and dr <= ROWS and
+            1 <= dc and dc <= COLS and
+            map[dr][dc] ~= "#")
+        then
+            stack[#stack + 1] = {dr, dc}
+        elseif (1 <= dr and dr <= ROWS and
+                1 <= dc and dc <= COLS and
+                map[dr][dc] == "#")
+        then
+            d = (d + 1) % 4
+            stack[#stack + 1] = {row, col}
+        else return false end
+    end
+    return false
+end
+
+local function countProducableCycles(m, r, c, d)
+    local map = m
+    local count = 0
+    local xCount = 0
+
+    for row, t in pairs(m) do
+        for col, v in pairs(t) do
+            if v == "X" then
+                xCount = xCount + 1
+                --print(xCount)
+                local testMap = copyMap(map)
+                testMap[row][col] = "#"
+                if checkForCycle(testMap, r, c, d) then
+                    count = count + 1
+                end
+            end
+        end
+    end
+    return count
+end
+
 local function countVisited(m)
     local count = 0
     for _, r in pairs(m) do
@@ -82,10 +153,18 @@ local map = getMap(lines)
 local startingPoint = getStartingPoint(map)
 local row, col, dir = startingPoint[1], startingPoint[2], startingPoint[3]
 local coverageMap = dfs(map, row, col, dir)
-for _, r in pairs(coverageMap) do
-    for _, c in pairs(r) do
-        io.write(c)
-    end
-    io.write("\n")
-end
+--for _, r in pairs(coverageMap) do
+--    for _, c in pairs(r) do
+--        io.write(c)
+--    end
+--    io.write("\n")
+--end
+--local tests = {{7, 4}, {8, 8}, {8, 7}, {9, 2}, {9, 4}, {10, 8}}
+--for _, test in pairs(tests) do
+--    print(test[1], test[2])
+--    local testMap = copyMap(map)
+--    testMap[test[1]][test[2]] = "#"
+--    print(checkForCycle(testMap, row, col, dir))
+--end
 print(countVisited(coverageMap))
+print(countProducableCycles(coverageMap, row, col, dir))
