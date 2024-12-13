87eb2cd7977208cb5318c05c41c604b82658b938
Nathan Coxson <nathancoxson2739@gmail.com>
2024-12-13 18:14:25 -0500

    added day ten part two solution

--- lua/q10/q10.lua
+++ lua/q10/q10.lua
@@ -22,7 +22,7 @@ local function getMap(filename)
     return lines
 end
 
-local function dfs(m, row, col)
+local function scoreTrailhead(m, row, col)
     assert(m[row][col] == 0, "Starting position is not 0")
     local ROWS, COLS = #m, #m[1]
     local stack = {{row, col, 0}}
@@ -54,10 +54,33 @@ local function dfs(m, row, col)
                 end
             end
         end
-        --for _, s in pairs(stack) do
-        --    io.write("{", s[1], ",", s[2], ",", s[3], "} ")
-        --end
-        --io.write(count, "\n")
+    end
+    return count
+end
+
+local function rateTrailhead(m, row, col)
+    assert(m[row][col] == 0, "Starting position is not 0")
+    local ROWS, COLS = #m, #m[1]
+    local stack = {{row, col, 0}}
+    local dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+    local count = 0
+
+    while #stack > 0 do
+        local state = table.remove(stack, #stack)
+        local r, c, h = state[1], state[2], state[3]
+
+        if h == 9 then count = count + 1
+        else
+            for _, dir in pairs(dirs) do
+                local dr, dc = r + dir[1], c + dir[2]
+                if (1 <= dr and dr <= ROWS and
+                    1 <= dc and dc <= COLS and
+                    m[dr][dc] == h + 1)
+                then
+                    stack[#stack+1] = {dr, dc, h + 1}
+                end
+            end
+        end
     end
     return count
 end
@@ -66,7 +89,17 @@ local function scoreTrailheads(m)
     local sum = 0
     for i, _ in pairs(m) do
         for j, _ in pairs(m[i]) do
-            if m[i][j] == 0 then sum = sum + dfs(m, i, j) end
+            if m[i][j] == 0 then sum = sum + scoreTrailhead(m, i, j) end
+        end
+    end
+    return sum
+end
+
+local function rateTrailheads(m)
+    local sum = 0
+    for i, _ in pairs(m) do
+        for j, _ in pairs(m[i]) do
+            if m[i][j] == 0 then sum = sum + rateTrailhead(m, i, j) end
         end
     end
     return sum
@@ -74,3 +107,4 @@ end
 
 local map = getMap(inputFile)
 print(scoreTrailheads(map))
+print(rateTrailheads(map))
