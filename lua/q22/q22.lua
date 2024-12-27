5430ea69bca0c5a63a5f198f6c51d37638fa7057
Nathan Coxson <nathancoxson2739@gmail.com>
2024-12-27 18:39:07 -0500

    added day twenty-two part two solution

--- lua/q22/q22.lua
+++ lua/q22/q22.lua
@@ -20,7 +20,7 @@ local function prune(n)
 end
 
 local function simulate(number, n)
-    for i = 1, n do
+    for _ = 1, n do
         -- Step 1
         number = prune(mix(number, number * 64))
 
@@ -33,7 +33,61 @@ local function simulate(number, n)
     return number
 end
 
+local function sequenceToString(seq)
+    local s = ""
+    for i, n in pairs(seq) do
+        s = s .. n
+        if i ~= #seq then s = s .. "," end
+    end
+    return s
+end
+
+local function getMaxBananas(nums, simulationLength)
+    local sequences = {}
+    for _, n in pairs(nums) do
+        local secretNumbers = {}
+        for _ = 1, simulationLength do
+            secretNumbers[#secretNumbers+1] = n
+            n = simulate(n, 1)
+        end
+
+        local prices = {}
+        for i = 1, #secretNumbers do
+            prices[#prices+1] = secretNumbers[i] % 10
+        end
+
+        local changes = {}
+        changes[1] = 0
+        for i = 2, #prices do
+            changes[i] = prices[i] - prices[i - 1]
+        end
+
+        local seqs = {}
+        for i = 2, simulationLength - 3 do
+            local seq = {}
+            for j = i, i + 3 do
+                seq[#seq+1] = changes[j]
+            end
+            local k = sequenceToString(seq)
+            if seqs[k] == nil then seqs[k] = prices[i + 3] end
+        end
+
+        for k, v in pairs(seqs) do
+            if sequences[k] == nil then sequences[k] = 0 end
+            sequences[k] = sequences[k] + v
+        end
+    end
+    local max = 0
+    for _, v in pairs(sequences) do
+        if v > max then max = v end
+    end
+    return max
+end
+
+
 local secretNumbers = getInput(inputFile)
 local sum = 0
 for _, n in pairs(secretNumbers) do sum = sum + simulate(n, 2000) end
 print("Part 1:", sum)
+
+print("Part 2:", getMaxBananas(secretNumbers, 2000))
